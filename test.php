1f3d8f802ce8db27e167e5645fd3635daa2aebf5
Ladislav Ondris <ladislav.ondris@seznam.cz>
2020-02-19 14:28:07 +0100

    html summary generation in test.php

--- test.php
+++ test.php
@@ -128,6 +128,10 @@ class TestSuiteResult
     public function getTestSuite() {
         return $this->testSuite;
     }
+
+    public function getTestCaseResults() {
+        return $this->testCaseResults;
+    }
 }
 
 class TestCase
@@ -152,19 +156,19 @@ class TestCase
         return $this->path . DIRECTORY_SEPARATOR . $this->name;
     }
 
-    private function getSourceFilename() {
+    public function getSourceFilename() {
         return $this->getPathname() . ".src";
     }
 
-    private function getInputFilename() {
+    public function getInputFilename() {
         return $this->getPathname() . ".in";
     }
     
-    private function getOutputFilename() {
+    public function getOutputFilename() {
         return $this->getPathname() . ".out";
     }
 
-    private function getReturnCodeFilename() {
+    public function getReturnCodeFilename() {
         return $this->getPathname() . ".rc";
     }
 }
@@ -228,10 +232,13 @@ class DirectoryTestSuiteReader implements ITestSuiteReader
 
     public function read() {
         $testSuites = array();
-
-        $dirs = $this->getSubDirectories($this->directory, $this->recursively);
-        array_push($dirs, $this->directory);
         
+        $dirs = array();
+        if ($this->recursively) {
+            $dirs = $this->getSubDirectories($this->directory);
+        }
+        array_push($dirs, $this->directory);
+
         foreach ($dirs as $dir) {
             $testCases = $this->getTestCases($dir);
             $testSuite = new TestSuite($dir, $testCases);
@@ -240,20 +247,19 @@ class DirectoryTestSuiteReader implements ITestSuiteReader
         return $testSuites;
     }
 
-    private function getSubDirectories($directory, $recursively) {
+    private function getSubDirectories($directory) {
         $dirs = array();
         $files = scandir($directory);
         foreach ($files as $file) {
             $dirPath = $directory . DIRECTORY_SEPARATOR . $file;
+            
             if (is_dir($dirPath)) {
                 if (basename($dirPath) == "." || basename($dirPath) == "..")
                     continue;
                 array_push($dirs, $dirPath);
 
-                if ($recursively) {
-                    $dirsRecursive = $this->getSubDirectories($dirPath, $recursively);
-                    $dirs = array_merge($dirsRecursive, $dirs);
-                }
+                $dirsRecursive = $this->getSubDirectories($dirPath);
+                $dirs = array_merge($dirsRecursive, $dirs);
             }
         }
         return $dirs;
@@ -320,9 +326,14 @@ class PreprocessTestSuiteReader implements ITestSuiteReader
 
 class HtmlSummaryGenerator implements ISummaryGenerator
 {
+    private $testSuiteResults;
+
     public function generate($testSuiteResults) {
-        $this->generateHeader();
-        $this->generateTestSuiteBlocks();
+        $this->testSuiteResults = $testSuiteResults;
+        $header = $this->generateHeader();
+        $testSuiteBlocks = $this->generateTestSuiteBlocks();
+        $html = $this->fillTemplate($header . $testSuiteBlocks);
+        file_put_contents("test_report.html", $html);
 
         // foreach ($testSuiteResults as $testSuiteResult) {
         //     echo "TestSuite \"{$testSuiteResult->getTestSuite()->getDirectory()}\"\n";
@@ -331,27 +342,156 @@ class HtmlSummaryGenerator implements ISummaryGenerator
         // }
     }
 
-    private function generateHeader() {
+    private function fillTemplate($content) {
+        return 
+        "<html>
+            <head>
+                <style>
+                    body {    
+                        max-width: 900px;
+                        margin: auto;
+                        font-family: \"Segoe UI\";
+                    }
 
+                    header {
+                        display: flex;
+                        justify-content: center;
+                        flex-flow: column;
+                        text-align: center;
+                    }
+
+                    #overallSummary {
+                        display: flex;
+                        justify-content: center;
+                        width: inherit;
+                        font-size: 28;
+                        background: #c8e1e9;
+                        border: 2px solid;
+                        padding: 5px;
+                    }
+
+                    #overallSummary table {
+                        width: 200px;
+                        font-size: 20px;
+                    }
+
+                    .directorySummary table {
+                        margin-top: 10px;
+                        font-weight: bold;
+                        font-size: 16px;
+                    }
+
+                    section table td {
+                        width: 200px;
+                    }
+
+                    .failed {
+                        color: red;
+                    }
+
+                    .passed {
+                        color: green;
+                    }
+                </style>
+            </head>
+            <body>
+                {$content}
+            </body>
+        </html>";
+    }
+
+    private function getTotalPassed() {
+        $passed = 0;
+        foreach ($this->testSuiteResults as $testSuiteResult) {
+            $passed += $testSuiteResult->getTotalPassed();
+        }
+        return $passed;
+    }
+
+    private function getTotalFailed() {
+        $failed = 0;
+        foreach ($this->testSuiteResults as $testSuiteResult) {
+            $failed += $testSuiteResult->getTotalFailed();
+        }
+        return $failed;
+    }
+
+    private function generateHeader() {
+        $passed = $this->getTotalPassed();
+        $failed = $this->getTotalFailed();
+        $totalTests = $passed + $failed;
+        return 
+        "<header>
+            <h1>Tests summary</h1>
+            <div id=\"overallSummary\">
+                <table>
+                    <tr>
+                        <td>Passed</td>
+                        <td>{$passed}</td>
+                    </tr>
+                    <tr>
+                        <td>Failed</td>
+                        <td>{$failed}</td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>{$totalTests}</td>
+                    </tr>
+                </table>
+            </div>
+            <hr />
+        </header>";
     }
 
     private function generateTestSuiteBlocks() {
-        foreach ($testSuiteResults as $testSuiteResult) {
-            $this->generateTestSuiteBlock($testSuiteResult);
+        $testSuiteBlocks = "<section>";
+        foreach ($this->testSuiteResults as $testSuiteResult) {
+            $testSuiteBlocks .= $this->generateTestSuiteBlock($testSuiteResult);
         }
+        return $testSuiteBlocks . "</section>";
     }
 
     private function generateTestSuiteBlock(TestSuiteResult $testSuiteResult) {
-        $this->generateTestCasesDetails();
-        $this->generateTestSuiteBlockSummary();
+        $testCasesDetails = $this->generateTestCasesDetails($testSuiteResult);
+        $testSuiteBlockSummary = $this->generateTestSuiteBlockSummary($testSuiteResult);
+        return "<div class=\"testSuite\">" . $testCasesDetails . $testSuiteBlockSummary . "</div><hr />";
     }
 
-    private function generateTestCasesDetails() {
-        
+    private function generateTestCasesDetails(TestSuiteResult $testSuiteResult) {
+        $testCaseDetails = "<h2 class=\"directory\">{$testSuiteResult->getTestSuite()->getDirectory()}</h2><table>";
+
+        foreach ($testSuiteResult->getTestCaseResults() as $testCaseResult) {
+            $evaluation = $testCaseResult->hasPassed() ? "Passed" : "Failed";
+            $testCaseDetails .= 
+            "<tr>
+                <td>{$testCaseResult->getTestCase()->getName()}</td>
+                <td class=\"{$evaluation}\">{$evaluation}</td>
+            </tr>";
+        }
+        return $testCaseDetails . "</table>";
     }
 
-    private function generateTestSuiteBlockSummary() {
-        
+    private function generateTestSuiteBlockSummary(TestSuiteResult $testSuiteResult) {
+        $passed = $testSuiteResult->getTotalPassed();
+        $failed = $testSuiteResult->getTotalFailed();
+        $total = $passed + $failed;
+
+        return "<div class=\"directorySummary\">
+                    <table>
+                        <tr>
+                            <td>Passed</td>
+                            <td>{$passed}</td>
+                        </tr>
+                        <tr>
+                            <td>Failed</td>
+                            <td>{$failed}</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>{$total}</td>
+                        </tr>
+                    </table>
+                </div>";
     }
 }
 
@@ -433,7 +573,7 @@ class TestRunner implements ITestRunner
     
     private function getParseCommand($test, $tmpOutFilename) {
         $srcFile =  $test->getSourceFilename();
-        return "D:\\Programs\\Xampp\\php\\php.exe -f {$this->parseScript} < \"{$srcFile}\" > \"{$tmpOutFilename}\"";
+        return "D:\\Programs\\Xampp\\php\\php.exe -f {$this->parseScript} < \"{$srcFile}\" > \"{$tmpOutFilename}\" 2> nul";
     }
 
     private function getInterpretCommand($test, $tmpOutFilename) {
